53ebb4298b909f75030006519f23d0a8591cb590
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-12 08:33:07 +0000

    added

--- ES6.js
+++ ES6.js
@@ -13,3 +13,28 @@ const students={
 const about =`My name is ${students.name} age of ${students.age} my hobbie is ${students.hobies} my favourite movie is${students.movies}`;
 
 console.log(about);
+
+// arrow function 
+
+// without parametter 
+const fiftyFive =()=> 55;
+// with parametter  
+const sixty = (num)=> num +60;
+// multiple parameter
+const addThree =(x,y,z)=> x+y+z;
+// multiline 
+const doMath =(num1, num2)=>{
+    const number =num1+num2;
+    return;
+}
+
+// spread operator  
+
+// when a copy same array using ... 
+const newNumbers =[...numbers];
+console.log(newNumbers);
+
+// create new array from older array and add a Element
+const currenrNumbers= [...numbers,45];
+
+console.log(currenrNumbers)
