4680996d17655d9ac6e6304b7cb44b4c8dc1a100
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-14 02:41:29 +0600

    edit

--- destructuring.js
+++ destructuring.js
@@ -1,6 +1,20 @@
-
+// 42-4 Array and object Destructuring, optional chaining
 
 // destructuring
 [x,y]=[52, 67];
 console.log(x);
+
+// object destructuring
+
+const students={
+    name:'Md Mahtab Uddin',
+    age:30,
+    hobies: ['travelling',' cycling'],
+    movies:['bahubali','kgf']
+};
+
+const [firstMovie, secondMovie] = students.movies;
+
+
+console.log(firstMovie);
 
